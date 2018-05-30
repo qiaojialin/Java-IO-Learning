3c2050eb68b80e9860f8aa99867ed0444780b996
qiaojialingithub <646274302@qq.com>
2018-05-30 08:09:29 +0800

    add file io stream

---
+++ src/main/java/learn/FileIOStreamTest.java
@@ -0,0 +1,67 @@
+package learn;
+
+import java.io.*;
+
+public class FileIOStreamTest {
+
+    private static final String fileName = "src/main/resources/test.myfile";
+    private static final File file = new File(fileName);
+
+    public static void main(String[] args) throws IOException {
+        if (file.exists())
+            file.delete();
+
+        //覆盖写入 6 个字节
+        write(new byte[]{1, 2, 3, 4, 5, 6}, false);
+
+        //FileInputStream只能顺序读
+        InputStream in = new FileInputStream(file);
+
+        byte[] array = new byte[3];
+        //读三个字节并打印
+        in.read(array);  print(array);
+        System.out.println("\n还剩 " + in.available() + " 个字节可读");
+
+        //再读三个字节并打印
+        in.read(array);  print(array);
+        System.out.println("\n没得读了：" + in.available());
+
+        //覆盖写入 2 个字节
+        write(new byte[]{7, 8}, false);
+        read();
+        //追加写入 3 个字节
+        write(new byte[]{1, 2, 3}, true);
+        read();
+    }
+
+    private static void print(byte[] array) {
+        for (byte b : array) {
+            System.out.print(b);
+        }
+    }
+
+    //将字节数组 a 以 mode 模式写入文件
+    private static void write(byte[] a, boolean mode) {
+        try (OutputStream out = new FileOutputStream(file, mode)) {
+            out.write(a);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //读取 10 个字节
+    public static void read() {
+        try (InputStream in = new FileInputStream(file)) {
+            byte[] a = new byte[10];
+            System.out.print("整个文件有 " + in.available() + " 个字节：");
+            in.read(a);
+            for (int i = 0; i < a.length; i++) {
+                System.out.print(a[i]);
+            }
+            System.out.println();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/learn/RandomAccessFileTest.java
@@ -0,0 +1,55 @@
+package learn;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class RandomAccessFileTest {
+    private static String fileName = "src/main/resources/test.myfile";
+    private static File file = new File(fileName);
+    public static void main(String[] args) throws IOException {
+        byte[] a = new byte[10];
+        RandomAccessFile randomAccess = new RandomAccessFile(file, "rw");
+        //跳过 2 个字节
+        randomAccess.seek(2);
+        //写入 6 个字节
+        randomAccess.write(new byte[]{1,2,3,4,5,6});
+        System.out.println("当前位置: " + randomAccess.getFilePointer());
+        System.out.println("文件长度: "+randomAccess.length());
+
+        //从头读取 10 个 bytes
+        randomAccess.seek(0);
+        randomAccess.read(a);
+        print(a);
+
+        //移动到 offset=3 的位置
+        randomAccess.seek(3);
+        System.out.println("当前位置: " + randomAccess.getFilePointer());
+        //写入一个byte，直接覆盖原来的数据
+        randomAccess.write(new byte[]{7});
+
+        //从头读取 10 个 bytes
+        randomAccess.seek(0);
+        randomAccess.read(a);
+        print(a);
+
+        //seek 到 offset=2 处
+        randomAccess.seek(2);
+        a = new byte[3];
+        //读 2 个字节到 a
+        randomAccess.read(a, 0, 2);
+        print(a);
+
+        randomAccess.close();
+        file.delete();
+    }
+
+
+    private static void print(byte[] array) {
+        System.out.print("读取：");
+        for (byte b : array) {
+            System.out.print(b);
+        }
+        System.out.println();
+    }
+}
